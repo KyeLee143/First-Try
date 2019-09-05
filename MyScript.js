fa60fbf67bd4480175a3ed871016d7087d8f2786
KyeLee143 <keilipalmares8455@gmail.com>
2019-09-05 08:37:05 -0700

    added script

---
+++ MyScript.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("#hide").click(function(){
+		$("#hide").hide();
+	});
+	$("img").click(function(){
+		$("p").show();
+	});
+	$(".stl").css({"color:seagreen"});
+});
